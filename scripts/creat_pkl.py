40e43bf34886c5007cb7706dbff65e1a0d226c0b
Aaron <chanman2841@gmail.com>
2023-07-18 22:26:15 -0500

    prep a test set of input data for training

--- scripts/creat_pkl.py
+++ scripts/creat_pkl.py
@@ -1,22 +1,23 @@
 import pickle
+import numpy as np
 
 def create_dct(n=10):
     tmp = {}
-    for i in range(n):
-        key = 'a'+str(i)
+    for i in range(1,n+1):
+        key = 'patient0000'+str(i)+'/study'+str(1)+'/view1'+'_frontal'
         tmp[key] = {
-            'pdesc': ['one two three'],
-            'bics': [1, 2],
-            'bts': [1, 2, 3, 4],
-            'label': [1, 0, 1, 0, 1, 0, 1, 0]
+            'pdesc': np.array([1, 12, 3, 4, 5]),
+            'bics': np.array([1, 2]),
+            'bts': np.array([1, 2, 3, 4]),
+            'label': np.array([1, 0, 1, 0, 1, 0, 1, 0])
         }
     
     return tmp
 
-def create_pckl(dct, filename='./data/predev/dct.pickle'):
+def create_pckl(dct, filename='./data/predev/dct.pkl'):
     with open(filename, 'wb') as handle:
         pickle.dump(dct, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-new = create_dct(10)
+new = create_dct(2)
 create_pckl(new)
 
--- train_model.py
+++ train_model.py
@@ -1,10 +1,13 @@
 import os
 import pickle
 import torch
+import numpy as np
+import argparse
 
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 from models.modeling_irene import IRENE, CONFIGS
+from torchvision import transforms, utils
 
 class Data(Dataset):
     def __init__(self, set_type, img_dir, transform=None, target_transform=None):
@@ -22,7 +25,7 @@ class Data(Dataset):
 
     def __getitem__(self, idx):
         k = self.idx_list[idx]
-        img_path = os.path.join(self.img_dir, k) + '.png'
+        img_path = os.path.join(self.img_dir, k) + '.jpg'
         img = Image.open(img_path).convert('RGB')
 
         label = self.mm_data[k]['label'].astype('float32')
@@ -36,3 +39,32 @@ class Data(Dataset):
         demo = torch.from_numpy(np.array(self.mm_data[k]['bics'])).float()
         lab = torch.from_numpy(self.mm_data[k]['bts']).float()
         return img, label, cc, demo, lab
+    
+def train_model():
+    data_transforms = {
+        'test': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+        ]),
+    }
+
+    img_dir = args.DATA_DIR
+
+    data = Data(args.SET_TYPE, img_dir, transform=data_transforms['test'])
+
+    return data
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('--CLS', action='store', dest='CLS', required=True, type=int)
+    parser.add_argument('--BSZ', action='store', dest='BSZ', required=True, type=int)
+    parser.add_argument('--DATA_DIR', action='store', dest='DATA_DIR', required=True, type=str)
+    parser.add_argument('--SET_TYPE', action='store', dest='SET_TYPE', required=True, type=str)
+    args = parser.parse_args()
+
+    data_tmp = train_model()
+    print(data_tmp.idx_list)
+    print(data_tmp.mm_data['patient00001/study1/view1_frontal'])
+    print(data_tmp[1])
