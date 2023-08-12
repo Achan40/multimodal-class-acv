7d6d6ea3aff12e90b03e9d0cc8402300bc9ece18
Aaron <chanman2841@gmail.com>
2023-08-12 08:03:13 -0500

    complete training for CNN

--- run.sh
+++ run.sh
@@ -13,5 +13,8 @@ python run_model.py --BSZ 24 --DATA_DIR ./data/actual/ train --EPCHS 10 --SAVED_
 # run testing 
 python run_model.py --BSZ 24 --DATA_DIR ./data/actual/ test --TST_LAB_SET ./data/actual/test_0.pkl --SAVED_MOD ./checkpoints/set_16_epc_1.pt
 
-# CNN
-python run_model_CNN.py --BSZ 3 --DATA_DIR ./data/predev/ train --EPCHS 5 --TRN_LAB_SET ./data/predev/train.pkl --VAL_LAB_SET ./data/predev/valid.pkl
+# CNN testing
+python run_model_cnn.py --BSZ 3 --DATA_DIR ./data/predev/ train --EPCHS 5 --TRN_LAB_SET ./data/predev/train.pkl --VAL_LAB_SET ./data/predev/valid.pkl
+
+# CNN run training loop
+python run_model_cnn.py --BSZ 24 --DATA_DIR ./data/actual/ train --EPCHS 10 --TRN_LAB_SET ./data/actual/train_0.pkl ./data/actual/train_1.pkl ./data/actual/train_2.pkl ./data/actual/train_3.pkl ./data/actual/train_4.pkl ./data/actual/train_5.pkl ./data/actual/train_6.pkl ./data/actual/train_7.pkl ./data/actual/train_8.pkl ./data/actual/train_9.pkl ./data/actual/train_10.pkl ./data/actual/train_11.pkl ./data/actual/train_12.pkl ./data/actual/train_13.pkl ./data/actual/train_14.pkl ./data/actual/train_15.pkl ./data/actual/train_16.pkl ./data/actual/train_17.pkl ./data/actual/train_18.pkl ./data/actual/train_19.pkl --VAL_LAB_SET ./data/actual/valid.pkl
