d17cb66b8c1fd285a3c9b1d20355ea2dd8cedbb1
Aaron <chanman2841@gmail.com>
2023-08-11 23:37:51 -0500

    add CNN architecture provided by team member

--- models/modeling_cnn.py
+++ models/modeling_cnn.py
@@ -1,29 +1,53 @@
 import torch
 import torch.nn as nn
 
+# Define hyperparameters for the CNN model
+hidden_units = 128
+dropout_rate = 0.5
+
 # Define the CNN architecture
 class MultiClassCNN(nn.Module):
     def __init__(self, num_classes=14):
         super(MultiClassCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(100352, 128)  # Adjust the input size based on your image dimensions
-        self.fc2 = nn.Linear(128, num_classes)
-        self.sigmoid = nn.Sigmoid()
-
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.batchnorm1 = nn.BatchNorm2d(32)
+        
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.dropout2 = nn.Dropout2d(p=dropout_rate)
+        self.batchnorm2 = nn.BatchNorm2d(64)
+        
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=(2, 2))
+        self.batchnorm3 = nn.BatchNorm2d(64)
+        
+        self.flatten = nn.Flatten()
+        self.dense1 = nn.Linear(50176, hidden_units)
+        self.batchnorm_dense = nn.BatchNorm1d(hidden_units)
+        self.dense2 = nn.Linear(hidden_units, 14)
+    
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.relu(out)
-        out = self.maxpool(out)
-        out = self.conv2(out)
-        out = self.relu(out)
-        out = self.maxpool(out)
-        out = out.view(out.size(0), -1)  # Flatten the tensor
-        out = self.fc1(out)
-        out = self.relu(out)
-        out = self.fc2(out)
-        out = self.sigmoid(out)
-        return out
-
+        x = self.conv1(x)
+        x = nn.ReLU()(x)
+        x = self.maxpool1(x)
+        x = self.batchnorm1(x)
+        
+        x = self.conv2(x)
+        x = nn.ReLU()(x)
+        x = self.maxpool2(x)
+        x = self.dropout2(x)
+        x = self.batchnorm2(x)
+        
+        x = self.conv3(x)
+        x = nn.ReLU()(x)
+        x = self.maxpool3(x)
+        x = self.batchnorm3(x)
+        
+        x = self.flatten(x)
+        x = self.dense1(x)
+        x = nn.ReLU()(x)
+        x = self.batchnorm_dense(x)
+        x = self.dense2(x)
+        
+        return torch.sigmoid(x)
